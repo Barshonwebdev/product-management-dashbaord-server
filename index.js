90b7e452ae4e877e99ce689de23782ce908ff5dd
Barshon <barshonweb@gmail.com>
2024-06-14 13:13:18 +0600

    my products api

--- index.js
+++ index.js
@@ -84,7 +84,16 @@ async function run() {
       const result=await productsCollection.find().toArray();
       res.send(result);
     })
+    app.get('/products/:email',async(req,res)=>{
+      const email=req.params.email;
+      const query={
+        seller_email:email
+      }
+      const result=await productsCollection.find(query).toArray();
+      res.send(result);
+    })
 
+    
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
